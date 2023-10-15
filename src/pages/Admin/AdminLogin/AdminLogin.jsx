41d29377056760467d5d85838efa792a22fb371d
amarilis <amarilispequeno@gmail.com>
2023-10-15 16:28:43 -0300

    atualiza icons

--- src/pages/Admin/AdminLogin/AdminLogin.jsx
+++ src/pages/Admin/AdminLogin/AdminLogin.jsx
@@ -1,20 +1,30 @@
 import { Link } from "react-router-dom";
 import styles from "./AdminLogin.module.css";
-import { BiLock, BiUser } from "react-icons/bi";
+import { MdOutlineEmail, BiUser } from "react-icons/bi";
 
 const AdminLogin = () => {
   return (
     <div className={styles.wrapper}>
       <form action="">
-        <h1>Bem Vindo</h1>
-        <div className={styles["input-box"]}>
-          <input type="email" placeholder="Email" required />
-          <BiUser />
+        <h1 className={styles.title}>Bem Vindo</h1>
+        <div className={styles.input}>
+          <input
+            type="email"
+            className="input-field"
+            placeholder="Email"
+            required
+          />
+          <BiUser className={styles.icon} />
         </div>
 
         <div className={styles["input-box"]}>
-          <input type="password" placeholder="Senha" required />
-          <BiLock />
+          <input
+            type="password"
+            className="input-field"
+            placeholder="Senha"
+            required
+          />
+          <MdOutlineEmail className={styles.icon} />
         </div>
 
         <button type="submit" className={styles.btn}>
@@ -23,7 +33,7 @@ const AdminLogin = () => {
 
         <div className={styles["register-link"]}>
           <p>
-            Não tem cadastro? <Link to="/">Cadastre</Link>
+            Não tem cadastro? <Link to="/">Cadastre-se</Link>
           </p>
         </div>
       </form>
