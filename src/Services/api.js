b5d289e17f69431b6358e902e2f82fb624f75506
Patrícia Vasques <patriciahhhh@gmail.com>
2023-10-15 15:45:01 -0300

    feat: Acrescente a API para cadastrar um produto

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 
+
 const api = axios.create({
     baseURL: process.env.URL_HOST_API, 
   });
@@ -19,3 +20,23 @@ export const register = async (name, email, password) => {
   const response = await api.post('/register', { name, email, password });
   return response.data;
 }
+
+export const cadastrarProduto = async (CadastrarProduto , authToken) => {
+  try {
+ 
+    const config = {
+      headers: {
+        'Authorization': `Bearer ${authToken}`,
+      },
+    };
+
+ 
+    const response = await axios.post("/products/admin", CadastrarProduto, config);
+
+    return response.data;
+  } catch (error) {
+ 
+    console.error("Erro ao cadastrar o produto:", error);
+    throw error; 
+  }
+};
