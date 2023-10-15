180e022d80e138b9b2c1858831581cdd24e05d5a
amarilis <amarilispequeno@gmail.com>
2023-10-15 00:26:05 -0300

    cria formulario login com icons

--- src/pages/Admin/AdminLogin/AdminLogin.jsx
+++ src/pages/Admin/AdminLogin/AdminLogin.jsx
@@ -1,12 +1,34 @@
+import { Link } from "react-router-dom";
 import styles from "./AdminLogin.module.css";
+import { BiLock, BiUser } from "react-icons/bi";
 
 const AdminLogin = () => {
-return (
-<div>
-<h1>AdminLogin</h1>
-</div>
-);
+  return (
+    <div className={styles.wrapper}>
+      <form action="">
+        <h1>Bem Vindo</h1>
+        <div className={styles["input-box"]}>
+          <input type="email" placeholder="Email" required />
+          <BiUser />
+        </div>
+
+        <div className={styles["input-box"]}>
+          <input type="password" placeholder="Senha" required />
+          <BiLock />
+        </div>
+
+        <button type="submit" className={styles.btn}>
+          Login
+        </button>
+
+        <div className={styles["register-link"]}>
+          <p>
+            Não tem cadastro? <Link to="/">Cadastre</Link>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default AdminLogin;
-
