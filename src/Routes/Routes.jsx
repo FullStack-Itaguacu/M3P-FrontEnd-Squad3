250452c06f70a628e49a3624a9494655724d5e90
ThiagoThe <nitrox2thi@gmail.com>
2023-10-14 22:57:55 -0300

    instalado dependencias e adicionado rota createUser

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,7 +1,8 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NotFound from '../pages/NotFound/NotFound';
-import Index from '../pages/Home/Index';
-import AdminLogin from '../pages/Admin/AdminLogin/AdminLogin';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NotFound from "../pages/NotFound/NotFound";
+import Index from "../pages/Home/Index";
+import AdminLogin from "../pages/Admin/AdminLogin/AdminLogin";
+import CreateUser from "../pages/Admin/Users/CreateUser";
 
 export default function App() {
   return (
@@ -9,7 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/login/admin" element={<AdminLogin />} />
-
+        <Route path="/admin/criar-usuario" element={<CreateUser />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
