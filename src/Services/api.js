a66ab678336b6df88c3777797d288fb7e5d604c3
amarilis <amarilispequeno@gmail.com>
2023-10-18 16:42:36 -0300

    console recebe email,  validaSenha está com erro

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,21 +1,30 @@
 import axios from "axios";
 
-
 const api = axios.create({
-    baseURL: process.env.URL_HOST_API, 
-  });
-  
-  export const login = async (email, password) => {
-    const response = await api.post('/login', { email, password });
-    return response.data;
+  baseURL: import.meta.env.VITE_URL_HOST_API,
+});
+
+export const login = async (email, password) => {
+  const response = await api.post("/login", { email, password });
+  return response.data;
+};
+
+export const loginAdmin = async (email, password) => {
+  try {
+    const response = await api.post("/user/admin/login", { email, password });
+
+    return response;
+  } catch (error) {
+    return error.response;
   }
-  
-  export const getProducts = async () => {
-    const response = await api.get('/products');
-    return response.data;
-  }
+};
+
+export const getProducts = async () => {
+  const response = await api.get("/products");
+  return response.data;
+};
 
 export const register = async (name, email, password) => {
-  const response = await api.post('/register', { name, email, password });
+  const response = await api.post("/register", { name, email, password });
   return response.data;
-}
+};
--- src/pages/Admin/AdminLogin/AdminLogin.jsx
+++ src/pages/Admin/AdminLogin/AdminLogin.jsx
@@ -1,30 +1,26 @@
 import styles from "./AdminLogin.module.css";
 import React, { useState } from "react";
 import { MdEmail, MdVpnKey } from "react-icons/md";
-import { useNavigate } from "react-router-dom";
+import { loginAdmin } from "../../../Services/api";
 
 function AdminLogin() {
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
   const [erroEmail, setErroEmail] = useState("");
   const [erroSenha, setErroSenha] = useState("");
-  const navigate = useNavigate();
 
   function validarEmail(email) {
     const valEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return valEmail.test(email);
-    console.log("validarEmail:", result);
-    return result;
   }
 
   function validarSenha(senha) {
-    const valSenha = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    const valSenha = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@#$%^&+=!])(?=\S+$).{8,}$/;
     return valSenha.test(senha);
-    console.log("validarSenha:", result);
-    return result;
+
   }
 
-  function validLog(e) {
+  async function validLog(e) {
     e.preventDefault();
 
     if (!validarEmail(email)) {
@@ -40,57 +36,71 @@ function AdminLogin() {
       return;
     }
     setErroSenha("");
-    navigate("/login/admin");
+
+    console.log("email", email);
+
+   // const data = ResponseLogin(response)
+
+    const response = await loginAdmin(email, senha);
   }
 
+    function ResponseLogin(response) {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+}
+
   return (
-    <div className="imagem-login">
-      <div className={styles["login-container"]}>
-        <form onSubmit={validLog} className={styles["formulario"]}>
-          <h1 className={styles["titulo"]}>Bem vindo</h1>
-          <div className={styles["input-container"]}>
-            <label htmlFor="email">e-mail</label>
-            <input
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              type="email"
-              name="email"
-              id="email"
-              placeholder="digite seu email"
-              autoComplete="off"
-            />
-            {erroEmail && (
-              <div className="alert alert-warning" role="alert">
-                {erroEmail}
-              </div>
-            )}
-            <MdEmail className={styles["icons"]} />
-            <label htmlFor="password">senha</label>
-            <input
-              required
-              value={senha}
-              onChange={(e) => setSenha(e.target.value)}
-              type="password"
-              name="password"
-              minLength="8"
-              id="password"
-              pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
-              placeholder="digite sua senha"
-            />
-            {erroSenha && (
-              <div className="alert alert-warning" role="alert">
-                {erroSenha}
-              </div>
-            )}
-            <MdVpnKey className={styles["icons"]} />
-          </div>
-          <input type="submit" value="Login" />
-        </form>
-        <div className={styles["register-container"]}>
-          <p>Não tem uma conta?</p>
-          <a href="#">Registrar</a>
-        </div>
+    <div className={styles.containerPrimario}>
+      <div className={styles.imagemContainer}>
+        <img src="/screen.png" alt="" />
       </div>
+      <h1 className={styles.tituloLogin}>Bem vindo</h1>
+      <form className={styles.formContainer} onSubmit={validLog}>
+        <label htmlFor="email">e-mail</label>
+
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          name="email"
+          id="email"
+          placeholder="digite seu email"
+          autoComplete="off"
+        />
+        {erroEmail && (
+          <div className={styles.alertWarning} role="alert">
+            {erroEmail}
+          </div>
+        )}
+        <div className={styles.icons}>
+          <MdEmail />
+        </div>
+        <label htmlFor="password">senha</label>
+        <input
+          required
+          value={senha}
+          onChange={(e) => setSenha(e.target.value)}
+          type="password"
+          name="password"
+          minLength="6"
+          id="password"
+          pattern="^(?=.*[A-Za-z])(?=.*\d)(?=.*[@#$%^&+=!])(?=\S+$).{8,}$"
+          placeholder="digite sua senha"
+        />
+        {erroSenha && (
+          <div className={styles.alertWarning} role="alert">
+            {erroSenha}
+          </div>
+        )}
+        <div className={styles.icons}>
+          <MdVpnKey />
+        </div>
+        <button className={styles.enviarLogin} type="submit" value="Entrar">
+          Entrar
+        </button>
+      </form>
     </div>
   );
 }
