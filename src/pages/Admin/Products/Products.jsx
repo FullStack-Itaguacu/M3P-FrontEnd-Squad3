ff9497fba2d968fbb102ebef1d8f802c5b04105d
Patrícia Vasques <patriciahhhh@gmail.com>
2023-10-22 11:44:13 -0300

    fix: coloquei o modal de produto cadastrado com sucesso e as mensagens de erros na página de cadastro de produtos

--- src/pages/Admin/Products/Products.jsx
+++ src/pages/Admin/Products/Products.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import Table from "../../../components/Table/Table"
 import useApi from "../../../hooks/useApi";
+import ModalSucess from "../../../components/Modal/ModalSucess"
 import styles from "./Products.module.css"
 
 
@@ -19,6 +20,8 @@ function NewProduct () {
     const { cadastrarProduto,uploadImage } = useApi();
     const [cadastroOk, setCadastroOk] = useState(false);
     const [carregandoImagem, setCarregandoImagem] = useState(false);
+    const [modalSuccessVisible, setModalSuccessVisible] = useState(false);
+    const [errorMessage, setErrorMessage] = useState(null);
 
     const product = {
       name: name,
@@ -57,6 +60,7 @@ function NewProduct () {
     setTotalStock('');
     setTypeProduct('');
     setDescription('');
+    setShowModal(true);
   };
 
 const [isUploading, setIsUploading] = useState(false);
@@ -70,7 +74,7 @@ const handleImageChange = async (e) => {
             setImageLink(response.links[0]);
             setCarregandoImagem(true);
         } catch (error) {
-            // Lide com erros, se necessário.
+            alert("Erro ao fazer upload da imagem!")
             console.error("Erro ao fazer upload da imagem:", error);
         }
     }
@@ -78,17 +82,29 @@ const handleImageChange = async (e) => {
 
 async function haddlerNewProduct(event) {
     event.preventDefault();
-    console.log(product);
-
+    console.log("haddlerNewProduct foi chamada");
+  
     if (!isUploading) {
+      try {
         const registerProduct = await cadastrarProduto(product);
-        console.log(registerProduct);
-        handleAddProduct();
-        handleOpenTable();
+        console.log("Resposta da API:", registerProduct); 
+  
+        if (registerProduct.status === 201) {
+          
+          handleAddProduct();
+          handleShowModalSucess()
+          handleOpenTable();
+        } else {
+            setErrorMessage("Produto já cadastrado");
+        }
+      } catch (error) {
+        console.error("Erro ao fazer a chamada para cadastrar o produto:", error);
+        setErrorMessage("Erro ao cadastrar o produto. Verifique sua conexão de rede.");
+      }
     } else {
-        console.log("Aguarde o upload da imagem ser concluído.");
+      console.log("Aguarde o upload da imagem ser concluído.");
     }
-}
+  }
 
 function handleOpenTable() {
   const newWindow = window.open('', '_blank', 'width=800,height=600');
@@ -99,100 +115,118 @@ function handleOpenTable() {
 async function haddlerNewProduct(event) {
     event.preventDefault();
     console.log(product);
-    const registerProduct = await cadastrarProduto(product);
-    console.log(registerProduct);
+    try {
+        const registerProduct = await cadastrarProduto(product);
 
-    if (registerProduct.status === 201) {
-        setCadastroOk(true);
+        if (registerProduct.status === 201) {
+            setCadastroOk(true);
+            setModalSuccessVisible(true);
+            
+        }
+    } catch (error) {
+        console.error(error);
+        
     }
 }
-            return (
-                <div className = {styles.fomulario}>
-                    <div>
-                    <h2 className={styles.titulo}>Cadastro de Produtos</h2>
-                    </div>
 
-                    <div className={styles.gridContainer}>
-
-                    <form onSubmit={haddlerNewProduct}>
-                            
-                        <div className={styles.grupo1}>
-                        <div>
-                            <label className={styles.labelNome} htmlFor="nomeProduto"> Nome do Produto:</label>
-                            <input className={styles.inputProduto} type="text" name="nome-produto" id="nome-produto" placeholder="Nome do produto"  onChange={(e) => setName(e.target.value)} required/>
-                        </div>
-
-                        <div className={styles.tipoMedicamento}>
-                            <label className={styles.labelTipoMedicamento} htmlFor="tipoProduto">Tipo Produto:</label>
-                            <select className={styles.selecDosagem}  name="tipo-produto" id="tipo-produto"   onChange={(e) => setTypeProduct(e.target.value)} required>
-                                <option value="">Selecione</option>
-                                <option value="Não controlado">Não controlado</option>
-                                <option value="Controlado">Controlado</option>
-                            </select>
-                        </div>
-
-                            <div>
-                            <label htmlFor="tipo-dosagem-produto">Tipo dosagem:</label>
-                            <select className={styles.inputTipoProduto}  name="tipo-dosagem" id="tipo-dosagem"   onChange={(e) => setTypeDosage(e.target.value)} required>
-                                <option value="">Selecione</option>
-                                <option value="mg">mg</option>
-                                <option value="mcg">mcg</option>
-                                <option value="g">g</option>
-                                <option value="ml">ml</option>
-                                <option value="%">%</option>
-                                <option value="outro">outro</option>
-                            </select>
-                        </div>
-                        
-                        </div>
-
-                        <div className={styles.grupo2}>
-                        <div >
-                            <label  htmlFor="dosagem-produto">Dosagem:</label>
-                            <input className={styles.inputProduto} type="text" name="dosagem-produto" id="dosagem-produto" placeholder="Dosagem"  onChange={(e) => setDosage(e.target.value)} required/>
-                        </div>
-                        <div>
-                            <label htmlFor="nome-laboratorio">Laboratório:</label>
-                            <input className={styles.inputProduto}  type="text" name="nome-laboratorio" id="nome-laboratorio" placeholder="Laboratório"   onChange={(e) => setLabName(e.target.value)} required/>
-                        </div>
-                        <div>
-                            <label htmlFor="descricao-produto">Descrição:</label>
-                            <textarea className={styles.inputDescricao}  name="descricao-produto" id="descricao-produto" placeholder="Descrição do medicamento"  onChange={(e) => setDescription(e.target.value)}/>
-                        </div>
-                        </div>
-
-                         <div className={styles.grupo3}>
-                        <div>
-                            <label  htmlFor="total-estoque">Quantidade:</label>
-                            <input className={styles.inputProduto} type="text" name="totalEstoque" id="totalEstoque" placeholder="Total estoque" onChange={(e) => setTotalStock(e.target.value)} required/>
-                        </div>
-                        <div>
-                            <label htmlFor="preco-produto">Preço: R$</label>
-                            <input className={styles.inputProduto} type="text" min="0" name="preco-produto" id="preco-produto" placeholder="Preço" onChange={(e) => setUnitPrice(e.target.value)} required/>
-                        </div>
-                        <div>
-                            <label htmlFor="imagemProduto">Imagem:</label>
-                            <input className={styles.inputImagem} type="file" name="imagem-produto" id="imagem-produto"
-                                accept="image/*" onChange={handleImageChange} required />
-                        </div>
-                        </div>
-
-                        <div>
-                        <button
-                            className={styles.buttonProdutos}
-                        >
-                            {carregandoImagem ? "Cadastrar" : "Aguarde o upload da imagem"}
-                        </button>
-
-                    </div>
-
-                </form>
-            </div>
-            {cadastroOk && <Table />}
+return (
+    <div className={styles.fomulario}>
+        <div>
+            <h2 className={styles.titulo}>Cadastro de Produtos</h2>
         </div>
-  );
-  
-            }
-            
 
-            export default NewProduct;
+        <div className={styles.gridContainer}>
+
+            <form onSubmit={haddlerNewProduct}>
+
+                <div className={styles.grupo1}>
+                    <div>
+                        <label className={styles.labelNome} htmlFor="nomeProduto"> Nome do Produto:</label>
+                        <input className={styles.inputProduto} type="text" name="nome-produto" id="nome-produto" placeholder="Nome do produto" onChange={(e) => setName(e.target.value)} required />
+                    </div>
+
+                    <div className={styles.tipoMedicamento}>
+                        <label className={styles.labelTipoMedicamento} htmlFor="tipoProduto">Tipo Produto:</label>
+                        <select className={styles.selecDosagem} name="tipo-produto" id="tipo-produto" onChange={(e) => setTypeProduct(e.target.value)} required>
+                            <option value="">Selecione</option>
+                            <option value="Não controlado">Não controlado</option>
+                            <option value="Controlado">Controlado</option>
+                        </select>
+                    </div>
+
+                    <div>
+                        <label htmlFor="tipo-dosagem-produto">Tipo dosagem:</label>
+                        <select className={styles.inputTipoProduto} name="tipo-dosagem" id="tipo-dosagem" onChange={(e) => setTypeDosage(e.target.value)} required>
+                            <option value="">Selecione</option>
+                            <option value="mg">mg</option>
+                            <option value="mcg">mcg</option>
+                            <option value="g">g</option>
+                            <option value="ml">ml</option>
+                            <option value="%">%</option>
+                            <option value="outro">outro</option>
+                        </select>
+                    </div>
+
+                </div>
+
+                <div className={styles.grupo2}>
+                    <div >
+                        <label htmlFor="dosagem-produto">Dosagem:</label>
+                        <input className={styles.inputProduto} type="text" name="dosagem-produto" id="dosagem-produto" placeholder="Dosagem" onChange={(e) => setDosage(e.target.value)} required />
+                    </div>
+                    <div>
+                        <label htmlFor="nome-laboratorio">Laboratório:</label>
+                        <input className={styles.inputProduto} type="text" name="nome-laboratorio" id="nome-laboratorio" placeholder="Laboratório" onChange={(e) => setLabName(e.target.value)} required />
+                    </div>
+                    <div>
+                        <label htmlFor="descricao-produto">Descrição:</label>
+                        <textarea className={styles.inputDescricao} name="descricao-produto" id="descricao-produto" placeholder="Descrição do medicamento" onChange={(e) => setDescription(e.target.value)} />
+                    </div>
+                </div>
+
+                <div className={styles.grupo3}>
+                    <div>
+                        <label htmlFor="total-estoque">Quantidade:</label>
+                        <input className={styles.inputProduto} type="text" name="totalEstoque" id="totalEstoque" placeholder="Total estoque" onChange={(e) => setTotalStock(e.target.value)} required />
+                    </div>
+                    <div>
+                        <label htmlFor="preco-produto">Preço: R$</label>
+                        <input className={styles.inputProduto} type="text" min="0" name="preco-produto" id="preco-produto" placeholder="Preço" onChange={(e) => setUnitPrice(e.target.value)} required />
+                    </div>
+                    <div>
+                        <label htmlFor="imagemProduto">Imagem:</label>
+                        <input className={styles.inputImagem} type="file" name="imagem-produto" id="imagem-produto"
+                            accept="image/*" onChange={handleImageChange} required />
+                    </div>
+                </div>
+
+                <div>
+                    <button
+                        className={styles.buttonProdutos}
+                    >
+                        {carregandoImagem ? "Cadastrar" : "Aguarde o upload da imagem"} 
+                    </button>
+
+                </div>
+
+            </form>
+        </div>
+
+        {errorMessage && (
+            <div className={styles.errorMessage}>
+            {errorMessage}
+        </div>
+)}
+
+
+        {modalSuccessVisible && (
+            <ModalSucess
+                showModal={modalSuccessVisible}
+                onCloseModal={() => setModalSuccessVisible(false)}
+            />
+        )}
+
+    </div>
+);
+}
+export default NewProduct
